13c4e2a0f6e3d0602b862206c05f57fda0b8d36f
AnatolyShirykalov <pipocavsobake@gmail.com>
2017-03-17 22:47:41 +0300

    try pip

--- 3/99_homework/1_pipeline/pipeline.go
+++ 3/99_homework/1_pipeline/pipeline.go
@@ -3,5 +3,13 @@ package pipeline
 type job func(in, out chan interface{})
 
 func Pipe(funcs ...job) {
+        cin := make(chan interface{},4)
+        cout := make(chan interface{},4)
+        for _, n := []interface{}{0, 30, 60, 90} {
+                cin <- n
+        }
+        for fn := range funcs {
+                go fn(cin, cout)
+        }
 	return
 }
